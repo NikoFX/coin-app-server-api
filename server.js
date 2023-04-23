01027d92d73e527fe0b7aa17b4f3495242ed735b
NikoFX <nikoofx@gmail.com>
2023-04-24 00:52:30 +0400

    initial commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql')
+const cors = require('cors')
+
+
+app.use(cors())
+app.use(express.static('src'))
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'coins'
+})
+
+connection.connect(err => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('mysql succesfully connected');
+    }
+})
+
+app.get('/search', (req, res) => {
+
+    const q = req.query.q ?? ''
+    const countryFilter = req.query.c ? `and country='${req.query.c}'` : ''
+    const typeFilter = req.query.t ? `and type='${req.query.t}'` : ''
+    const metalFilter = req.query.m ? `and metal='${req.query.m}'` : ''
+    const qualityFilter = req.query.qt ? `and quality='${req.query.qt}'` : ''
+    const priceFilter = (req.query.pFrom | req.query.pTo) ? `and price between'${req.query.pFrom || 0}' and '${req.query.pTo || 99999}'` : ''
+    const yearFilter = (req.query.yFrom | req.query.yTo) ? `and year between'${req.query.yFrom || 0}' and '${req.query.yTo || 99999}'` : ''
+
+    const query = `select * from coins where (name like '%${q}%' ${typeFilter} ${countryFilter} ${metalFilter} ${qualityFilter} ${priceFilter} ${yearFilter})`
+    connection.query(query, (err, rows) => {
+        if (err) throw err
+
+        res.json(rows)
+    })
+
+})
+
+app.get('/description/:id',(req,res)=>{
+    const query = `select * from coins where id=${req.params.id}`
+    connection.query(query,(err,rows)=>{
+        if(err) throw err
+
+        res.json(rows[0])
+    })
+})
+
+app.listen(8080, () => {
+    console.log('8080 server is running');
+})
